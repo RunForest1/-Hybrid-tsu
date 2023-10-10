3eea4ef15a89c96fedbf9ba065766e23037fbe00
RunForest <l-n-w@mail.ru>
2023-10-10 23:01:48 +0700

    second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["./**/*.html"],
   theme: {
-    extend: {},
+    container: {
+      padding: '160px',
+      center: true
+    },
+    extend: {
+      blue: {
+        'primary': '#3B82F6'
+      },
+      white: {
+        'primary': '#FFFFFF'
+      },
+      darks: {
+        'primary': '#0B0C0D'
+      },
+      lightdark: {
+        'primary': '#D9D9D933'
+      },
+      fontFamily: {
+        'euclid': ['Euclid Circular B', 'sans-serif'],
+        'poppins': ['Poppins', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
